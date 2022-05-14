814e7a10acdea0b2b3446f123798d6fdb83e2f5c
Aurelien CABIAC <acabiac@e2r5p11.clusters.42paris.fr>
2022-05-14 18:56:41 +0200

    second push

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 #include <fstream>
 #include <random>
 
+#define BACKGROUND_DEFAULT "\033[49m"
+#define FOREGROUND_DEFAULT "\033[39m"
+#define BACKGROUND_GREEN "\033[42m"
+#define FOREGROUND_GREEN "\033[32m"
+#define BACKGROUND_YELLOW "\033[103m"
+#define FOREGROUND_YELLOW "\033[33m"
+#define BACKGROUND_GREY "\033[37m"
+#define FOREGROUND_GREY "\033[47m"
+
 bool	fillDictionnary( std::unordered_set< std::string > & dictionnary ) {
 
 	std::ifstream	infile;
@@ -32,11 +41,81 @@ bool	fillDictionnary( std::unordered_set< std::string > & dictionnary ) {
 	return true;
 }
 
-void	printGrid( std::vector< std::string > const & grid ) {
+#define GREY 0
+#define YELLOW 1
+#define GREEN 2
+
+void	printWord( std::string const & word, std::string const & answer ) {
+
+	int colors[5] = { 0 };
+
+	int letters_count[5] = { 0 };
+
+	for ( std::string::size_type i = 0 ; i < answer.size() ; i ++) {
+	
+		for ( std::string::size_type j = 0  ; j < answer.size() ; j++) {
+
+			if (answer[i] == answer[j])
+				letters_count[i]++;
+		}
+	}
+	
+	for ( std::string::size_type i = 0 ; i < answer.size() ; i ++) {
+	
+			if (answer[i] == word[i]) {
+				letters_count[i]--;
+				colors[i] = GREEN;
+				for ( std::string::size_type j = i + 1  ; j < answer.size() ; j++) {
+
+					if (answer[i] == answer[j])
+						letters_count[j]--;
+				}
+			}
+	}
+
+	for ( std::string::size_type i = 0 ; i < answer.size() ; i++) {
+	
+		for ( std::string::size_type j = 0  ; j < answer.size() ; j++) {
+
+			if ( word[i] == answer[j] && colors[i] != GREEN && letters_count[i] != 0 ) {
+				
+				letters_count[i]--;
+				colors[i] = YELLOW;
+				for ( std::string::size_type j = i + 1  ; j < answer.size() ; j++) {
+
+					if (word[i] == answer[j])
+						letters_count[j]--;
+				}
+			}
+		}
+	}
+	
+	for (std::string::size_type i = 0; i < answer.size() ; i++ ) {
+
+		switch (colors[i]) {
+
+			case GREEN :
+				std::cout << BACKGROUND_GREEN;
+				break;
+			case YELLOW :
+				std::cout << BACKGROUND_YELLOW;
+				break;
+			default :
+				break;
+		}
+		std::cout << word[i] << BACKGROUND_DEFAULT;
+		if ( i != 4 )
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return ;
+}
+
+void	printGrid( std::vector< std::string > const & grid, std::string const & answer ) {
 
 	for ( std::vector< std::string >::const_iterator it = grid.begin() ; it != grid.end() ; ++it ) {
 
-		std::cout << *it << std::endl;
+		printWord( *it, answer );
 	}
 
 	return;
@@ -58,6 +137,8 @@ int	main() {
 	std::uniform_int_distribution<std::mt19937::result_type> dist( 1, dictionnary.size() );
 	size_t lol = dist(rng);
 
+	std::cout << BACKGROUND_GREEN << "LOLOLOL" << BACKGROUND_DEFAULT << std::endl;
+
 	std::cout << "lol is " << lol << std::endl;
 
 	{
@@ -72,7 +153,7 @@ int	main() {
 
 	for ( size_t i = 0 ; i < 6 ; ++i ) {
 
-		grid.push_back("_ _ _ _ _");
+		grid.push_back("_____");
 	}
 	/* testing if parsing of dictionnary went fine
 	std::unordered_set<std::string>::iterator it = dictionnary.find("zappy");
@@ -91,7 +172,7 @@ int	main() {
 
 	std::cout << "Total words available: " << dictionnary.size() << std::endl;
 
-	printGrid( grid );
+	printGrid( grid, answer );
 
 	do {
 
@@ -108,7 +189,8 @@ int	main() {
 			else if ( it == dictionnary.end() )
 				std::cout << "The word is not in the word list" << std::endl;
 		} while ( std::cin.eof() == 0 &&  ( input.size() != 5 || it == dictionnary.end() ) );
-//		actualizeGrid( answer, input, grid[guess - 1] );
+		grid[guess - 1] = input;
+		printGrid(grid, answer);
 		guess++;
 	} while ( guess < 7 && std::cin.eof() == 0 );
 
