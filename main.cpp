7f0e7ee4578ee03aab39f771a06d550ca7ad9d8f
Aurelien CABIAC <acabiac@e2r5p16.clusters.42paris.fr>
2022-05-15 19:29:06 +0200

    started refacto

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <fstream>
 #include <random>
 #include <algorithm>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define FOREGROUND_DEFAULT "\033[39m"
 #define FOREGROUND_WHITE "\033[37m"
@@ -19,6 +22,27 @@
 #define YELLOW 1
 #define GREEN 2
 
+class RandomGen {
+
+	protected :
+
+		std::random_device	rd;
+		std::mt19937		rng;
+		std::uniform_int_distribution<std::mt19937::result_type> dist;
+
+	public :
+
+		RandomGen( size_t min = 1, size_t max = 1) : rng ( std::mt19937 ( rd() ) ),  dist (std::uniform_int_distribution<std::mt19937::result_type> ( min, max )) {
+
+			return;
+		}
+
+		size_t	getRandomValue() {
+
+			return dist(rng);
+		}
+};
+
 bool	fillDictionnary( std::unordered_set< std::string > & dictionnary , int ac, char **av ) {
 
 	std::ifstream	infile;
@@ -27,21 +51,8 @@ bool	fillDictionnary( std::unordered_set< std::string > & dictionnary , int ac, 
 	infile.open( "words.txt", std::ifstream::in );
 	if ( infile.is_open() == true ) {
 
-		do {
-
-			str.clear();
-			getline( infile, str );
-			if ( str.size() == 5 && std::all_of( str.begin(), str.end(), []( char c ) { return std::isalpha(c); } ) )
-				dictionnary.insert(str);
-		} while ( infile );
-
-		infile.close();
-	}
-
-	for ( int i = 1 ; i < ac ; ++i ) {
-
-		infile.open( av[i], std::ifstream::in );
-		if ( infile.is_open() == true ) {
+		struct stat stats;
+		if ( stat( "words.txt", &stats ) != -1 && (stats.st_mode & S_IFMT) == S_IFREG ) {
 
 			do {
 
@@ -50,7 +61,27 @@ bool	fillDictionnary( std::unordered_set< std::string > & dictionnary , int ac, 
 				if ( str.size() == 5 && std::all_of( str.begin(), str.end(), []( char c ) { return std::isalpha(c); } ) )
 					dictionnary.insert(str);
 			} while ( infile );
+		}
+		infile.close();
+	}
 
+	for ( int i = 1 ; i < ac ; ++i ) {
+
+		infile.open( av[i], std::ifstream::in );
+
+		if ( infile.is_open() == true ) {
+
+			struct stat stats;
+			if ( stat( av[i], &stats ) != -1 && (stats.st_mode & S_IFMT) == S_IFREG ) {
+
+				do {
+
+					str.clear();
+					getline( infile, str );
+					if ( str.size() == 5 && std::all_of( str.begin(), str.end(), []( char c ) { return std::isalpha(c); } ) )
+						dictionnary.insert(str);
+				} while ( infile );
+			}
 			infile.close();
 		}
 	}
@@ -77,8 +108,11 @@ void	setYellow( std::vector< int > & colors, std::string const & word, std::stri
 
 	std::string::const_iterator answerIt = answer.begin();
 	
-	for ( ; answerIt != answer.end(); ++answerIt ) {
+	
+	for ( std::vector< int>::iterator answerColorsIt = colors.begin(); answerIt != answer.end(); ++answerIt, ++answerColorsIt ) {
 		
+		if ( *answerColorsIt == GREEN)
+			continue;
 		std::vector< int>::iterator colorsIt = colors.begin();
 		for ( std::string::const_iterator wordIt = word.begin() ; wordIt != word.end() && colorsIt != colors.end() ; ++wordIt, ++colorsIt ) {
 
@@ -100,16 +134,12 @@ void	setColors( std::vector< int > & colors, std::string const & word, std::stri
 	setYellow( colors, word, answer );
 }
 
-void	printWord( std::string word, std::string const & answer ) {
+void	printWord( std::vector<int> const & colors, std::string const & word ) {
 
-	std::vector<int> colors;
-
-	colors.insert( colors.end(), 5, GREY);
-	setColors(colors, word, answer);
-
-	for_each( word.begin(), word.end(), [](char & c) {c = std::toupper(c);} );
-	std::vector<int>::iterator colorsIt = colors.begin();
-	for (std::string::iterator it = word.begin() ; it != word.end() && colorsIt != colors.end(); ++it, ++colorsIt) {
+	std::string	tmp (word);
+	for_each( tmp.begin(), tmp.end(), [](char & c) {c = std::toupper(c);} );
+	std::vector<int>::const_iterator colorsIt = colors.begin();
+	for (std::string::iterator it = tmp.begin() ; it != tmp.end() && colorsIt != colors.end(); ++it, ++colorsIt) {
 
 		switch ( *colorsIt ) {
 
@@ -131,80 +161,13 @@ void	printWord( std::string word, std::string const & answer ) {
 	return ;
 }
 
-void	printWordblabla( std::string & word, std::string const & answer ) {
+void	printGrid( std::vector< std::vector< int > > const & colorsVec, std::vector< std::string > const & grid ) {
 
-	int colors[5] = { 0 };
-
-	int letters_count[5] = { 0 };
-
-	for ( std::string::size_type i = 0 ; i < answer.size() ; i ++) {
-	
-		for ( std::string::size_type j = 0  ; j < answer.size() ; j++) {
-
-			if (answer[i] == answer[j])
-				letters_count[i]++;
-		}
-	}
-	
-	for ( std::string::size_type i = 0 ; i < answer.size() ; i ++) {
-	
-			if (answer[i] == word[i]) {
-				colors[i] = GREEN;
-				for ( std::string::size_type j = 0  ; j < answer.size() ; j++) {
-
-					if (word[i] == answer[j])
-						letters_count[j]--;
-				}
-			}
-	}
-
-	for ( std::string::size_type i = 0 ; i < answer.size() ; i++) {
-	
-		for ( std::string::size_type j = 0  ; j < answer.size() ; j++) {
-
-			if ( word[i] == answer[j] && colors[i] != GREEN && letters_count[j] > 0 ) {
-				
-				colors[i] = YELLOW;
-				letters_count[j]--;
-				for ( std::string::size_type k = j + 1  ; k < answer.size() ; k++) {
-
-					if (answer[j] == answer[k] && letters_count[k] > 0 && colors[k] != GREEN )
-						letters_count[k]--;
-				}
-			}
-		}
-	}
-
-	std::for_each(word.begin(), word.end(), [](char & c) { c = std::toupper(c); } );
-	for (std::string::size_type i = 0; i < answer.size() ; i++ ) {
-
-		switch (colors[i]) {
-
-			case GREEN :
-				std::cout << FOREGROUND_GREEN;
-				break;
-			case YELLOW :
-				std::cout << FOREGROUND_YELLOW;
-				break;
-			default :
-				std::cout << FOREGROUND_GREY;
-				break;
-		}
-		std::cout << FOREGROUND_BOLD  << word[i] << FOREGROUND_BOLD << FOREGROUND_WHITE;
-		if ( i != 4 )
-			std::cout << " ";
-	}
-	std::cout << std::endl;
-	std::for_each(word.begin(), word.end(), [](char & c) { c = std::tolower(c); } );
-	return ;
-}
-
-void	printGrid( std::vector< std::string > & grid, std::string const & answer ) {
-
-	for ( std::vector< std::string >::iterator it = grid.begin() ; it != grid.end() ; ++it ) {
+	std::vector< std::vector< int > >::const_iterator colorsVecIt = colorsVec.begin();
+	for ( std::vector< std::string >::const_iterator it = grid.begin() ; it != grid.end() ; ++it, ++colorsVecIt ) {
 
 		std::cout << '\t';
-		printWord( *it, answer );
+		printWord( *colorsVecIt, *it);
 		std::cout << std::endl;
 	}
 
@@ -224,12 +187,9 @@ int	main(int ac, char **av) {
 	}
 	std::vector< std::string > grid;
 	std::string	answer;
-	std::random_device	rd;
-	std::mt19937 rng( rd() );
-	std::uniform_int_distribution<std::mt19937::result_type> dist( 1, dictionnary.size() );
-	size_t lol = dist(rng);
+	RandomGen rGen ( 1, dictionnary.size() );
+	size_t lol = rGen.getRandomValue() ;
 
-	std::cout << FOREGROUND_WHITE;
 	{
 		
 		std::unordered_set<std::string>::iterator it = dictionnary.begin();
@@ -237,16 +197,18 @@ int	main(int ac, char **av) {
 			;
 		answer = *it;
 	}
+	std::vector< std::vector<int> > colorsVec;
+	colorsVec.insert( colorsVec.end(), 6, std::vector<int> () );
+	for_each( colorsVec.begin(), colorsVec.end(), []( std::vector<int> & vec ) { vec.insert( vec.end(), 5, GREY ); } );
+
+	std::cout << FOREGROUND_WHITE;
 	std::cout << "answer is " << answer << std::endl;
 
-	for ( size_t i = 0 ; i < 6 ; ++i ) {
-
-		grid.push_back("_____");
-	}
+	grid.insert( grid.end(), 6, std::string ( "______" ) );
 
 	std::cout << "Total words available: " << dictionnary.size() << std::endl;
 
-	printGrid( grid, answer );
+	printGrid( colorsVec, grid );
 
 	do {
 
@@ -262,7 +224,7 @@ int	main(int ac, char **av) {
 			if (input.size() != 5 ) {
 
 				std::cout << "The word must be five letter long!" << std::endl;
-				printGrid( grid, answer );
+				printGrid( colorsVec, grid );
 				std::cout << "input: ";
 				continue;
 			}
@@ -270,14 +232,15 @@ int	main(int ac, char **av) {
 			it = dictionnary.find( input );
 			if ( it == dictionnary.end() ) {
 				std::cout << "The word is not in the word list" << std::endl;
-				printGrid( grid, answer );
+				printGrid( colorsVec, grid );
 				std::cout << "input: ";
 			}
 		} while ( std::cin.eof() == 0 &&  ( input.size() != 5 || it == dictionnary.end() ) );
 		if (std::cin.eof() == 1)
 			break;
 		grid[guess] = input;
-		printGrid(grid, answer);
+		setColors( *(colorsVec.begin() + guess), grid[guess], answer);
+		printGrid(colorsVec, grid );
 		if (input == answer)
 			break;
 		guess++;
